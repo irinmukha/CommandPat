01a131237f741432d9322f52e2fde835edca9f53
irinmukha <irin.mukha@gmail.com>
2013-12-19 18:55:43 +0300

    initial commit

---
+++ src/CeilingFanOffCommand.java
@@ -0,0 +1,35 @@
+import definition.CeilingFan;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Irin
+ * Date: 19.12.13
+ * Time: 18:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class CeilingFanOffCommand implements Command {
+    CeilingFan ceilingFan;
+    int prevSpeed;
+
+    public CeilingFanOffCommand(CeilingFan ceilingFan) {
+        this.ceilingFan = ceilingFan;
+    }
+
+    @Override
+    public void execute() {
+        ceilingFan.off();
+    }
+
+    @Override
+    public void undo() {
+        if (prevSpeed == CeilingFan.HIGH) {
+            ceilingFan.high();
+        } else if (prevSpeed == CeilingFan.MEDIUM) {
+            ceilingFan.medium();
+        } else if (prevSpeed == CeilingFan.LOW) {
+            ceilingFan.low();
+        } else if (prevSpeed == CeilingFan.OFF) {
+            ceilingFan.off();
+        }
+    }
+}
